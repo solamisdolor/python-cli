405f40ed150c798f67bd9bbbd6f698a962b73220
ab <solamisdolor@gmail.com>
2018-06-19 12:26:05 +1000

    implement loadbase64

--- commands/_plgate.py
+++ commands/_plgate.py
@@ -28,22 +28,29 @@ class _plgate(object):
     def dumpbase64(self):
         return base64.urlsafe_b64encode(self.dumps().encode("utf-8"))
 
-    @staticmethod
-    def loadbase64(string):
-        return base64.urlsafe_b64decode(string)
+    def loadbase64(self, string):
+        s = base64.urlsafe_b64decode(string).decode("utf-8")
+        self.__dict__ = json.loads(s)
 
     def checksum(self):
         return hashlib.sha256(self.dumps().encode("utf-8")).hexdigest()
 
-    def to_artifactory_string(self):
+    def to_art_string(self, prefix=""):
+        s = "properties="
+        s += "{}{}={}".format(prefix, self.id, self.dumpbase64())
+        s += ";{}{}_CHKSUM={}".format(prefix, self.id, self.checksum())
+        return s
+
+
+    def old_to_artifactory_string(self, prefix="", postfix=""):
         """
         /api/storage/{repoKey}/{itemPath}?properties=p1=v1[,v2][|p2=v3][&recursive=1]
         Returns the querystring part
         """
-        s="properties="
-        s += "{}={};".format(self.id, self.value)
-        s += "{}_DTSTAMP={};".format(self.id, self.dtstamp)
-        s += "{}_CHKSUM={}".format(self.id, self.checksum())
+        s = "properties="
+        s += "{}{}{}={}".format(prefix, self.id, postfix, self.value)
+        s += ";{}{}_DTSTAMP{}={}".format(prefix, self.id, postfix, self.dtstamp)
+        s += ";{}{}_CHKSUM{}={}".format(prefix, self.id, postfix, self.checksum())
         return s
 
     def from_artifactory_json(self):
@@ -55,8 +62,16 @@ class _plgate(object):
 class _plgateOverride(_plgate):
     """An override"""
 
-    def __init__(self, gate_id, value, by):
+    def __init__(self, gate_id=None, value=None, by=None):
         super(_plgateOverride, self).__init__(gate_id, value)
         self.by = by
 
-    
+
+    def to_art_string(self):
+        s = super().to_art_string(prefix="_")
+        return s
+
+    def old_to_artifactory_string(self):
+        s = super().to_artifactory_string(prefix="_")
+        return s + ";{}{}_BY{}_{}".format("_", self.id, "", self.by)
+
--- commands/plgatetest.py
+++ commands/plgatetest.py
@@ -12,15 +12,22 @@ class plgatetest(base):
         gate = _plgate(gateID.TEST_REGR, "pass")
 
         print (gate.dumps())
-
         print (gate.checksum())
-
-        print (gate.to_artifactory_string())
+        print (gate.to_art_string())
 
         gate_override = _plgateOverride(gateID.TEST_REGR, "fail", "the overlord")
 
+        print ("---override---")
+
         print (gate_override.dumps())
         print (gate_override.checksum())
+        print (gate_override.to_art_string())
+
+        print ("---using base64---")
+
         s = gate_override.dumpbase64()
         print (s)
-        print (_plgate.loadbase64(s))
+        reloaded = _plgateOverride()
+        reloaded.loadbase64(s)
+        print (reloaded.__dict__)
+        print (reloaded.to_art_string())
